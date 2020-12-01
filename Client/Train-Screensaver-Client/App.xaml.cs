aa9c2e9fc4627a50e2cd5431d34cf178bd1448ec
Jan Bronec <49397215+XelfXendr@users.noreply.github.com>
2020-12-01 18:10:41 +0100

    Screensaver groundwork
    Prepared some groundwork for screensaver.
    App takes arguments on startup (provided by OS).
    /s - starts u the ScreensaverWindow
    /c, /p - TODO

--- Client/Train-Screensaver-Client/App.xaml.cs
+++ Client/Train-Screensaver-Client/App.xaml.cs
@@ -13,5 +13,57 @@ namespace Train_Screensaver_Client
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                string first = e.Args[0].ToLower().Trim(); //first argument (/c, /p or /s)
+                string second = null; //second argument - int specifying window handle
+
+                //Parsing args
+                if (first.Length > 2)
+                {
+                    second = first.Substring(3).Trim();
+                    first = first.Substring(0, 2);
+                }
+                else if (e.Args.Length > 1)
+                    second = e.Args[1];
+
+                //processing args
+                switch (first)
+                {
+                    case "/c":  //Show configuration form
+                        ShowConfiguration();
+                        break;
+
+                    case "/p":  //Show screen-saver preview
+                        //Preview not yet supported
+                        break;
+
+                    case "/s":  //Show screen-saver
+                        ShowScreenSaver();
+                        break;
+
+                    default: //Invalid
+                        MessageBox.Show("Command-line argument \"" + first + "\" is not valid.", "ScreenSaver", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                        break;
+                }
+            }
+            else
+                ShowConfiguration();
+        }
+
+        static void ShowScreenSaver()
+        {
+            var window = new ScreensaverWindow();
+            window.WindowState = WindowState.Maximized;
+            window.WindowStyle = WindowStyle.None;
+            window.Show();
+        }
+
+        static void ShowConfiguration()
+        {
+            MessageBox.Show("Screensaver cannot be configured yet.", "ScreenSaver", MessageBoxButton.OK);
+        }
     }
 }
---
+++ Client/Train-Screensaver-Client/ScreensaverWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Train_Screensaver_Client
+{
+    /// <summary>
+    /// Interakční logika pro ScreensaverWindow.xaml
+    /// </summary>
+    public partial class ScreensaverWindow : Window
+    {
+        private Point mousePos = new Point(-1, -1);
+
+        public ScreensaverWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.None;
+            Topmost = true;
+        }
+
+        //Stop screensaver if some event happens
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pos = e.GetPosition(this);
+            if (mousePos.X != -1 || mousePos.Y != -1)
+            {
+                if (Math.Abs(pos.X - mousePos.X) > 5 || Math.Abs(pos.Y - mousePos.Y) > 5)
+                    StopScreenSaver();
+            }
+            mousePos = pos;
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            => StopScreenSaver();
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+            => StopScreenSaver();
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+            => StopScreenSaver();
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+            => StopScreenSaver();
+
+        public void StopScreenSaver()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
